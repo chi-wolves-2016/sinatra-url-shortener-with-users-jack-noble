9fb125d34e169f766eb626e72590de54701fa02c
Jack Noble <email": null>
2016-09-15 15:10:34 -0500

    add counter column

--- db/migrate/20160915144558_create_urls.rb
+++ db/migrate/20160915144558_create_urls.rb
@@ -3,6 +3,7 @@ class CreateUrls < ActiveRecord::Migration
     create_table :urls do |t|
       t.string :long_url, null: false
       t.string :short_url, null: false
+      t.integer :counter, null: false, default: 0
       t.integer :creator_id
 
       t.timestamps(null: false)
