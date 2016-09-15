c934606cc48dbc02cde73078a0704bd02db3af1a
Jack Noble <email": null>
2016-09-15 16:48:06 -0500

    Push up half done

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,7 @@
 get '/' do
   redirect "/urls"
 end
+
+get '/session-viewer' do
+  session.inspect
+end
--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,1 +1,27 @@
+get '/sessions/new' do
+  erb :'/sessions/new'
+end
+
+post '/sessions' do
+  @user = User.find_by(email: params[:email])
+  if @user
+    if @user.authenticate(params[:password])
+       session[:user_id] = @user.id
+       redirect '/urls'
+    else
+      @errors = ["Wrong Email or Password"]
+      erb :'/sessions/new'
+    end
+  else
+    @errors = ["Wrong Email or Password"]
+    erb :'/sessions/new'
+  end
+end
+
+delete '/sessions' do
+  if session[:user_id]
+    session.delete(:user_id)
+    redirect '/urls'
+  end
+end
 
